af2a250feffce73167d8bfef2576ac7ea80a8a64
Andreas Neustifter <andreas.neustifter@gmail.com>
2014-10-14 22:16:06 +0200

    Added comments to german_fuzzy_text module.

--- src/german_fuzzy_text.c
+++ src/german_fuzzy_text.c
@@ -1,30 +1,41 @@
 #include <stdio.h>
 
+// Create static buffer to return to caller.
 #define MAXLEN 80
 static char retval[MAXLEN];
 
+// Helper function for appending strings together.
 static void addstring(char* dest, char* src, int* used, int* free) {
     (*used) += snprintf(dest + (*used), (*free), "%s", src);
     (*free) = MAXLEN - 1 - (*used);
 }
 
+// Takes hours and minutes and converts them to a German time text.
 char* german_fuzzy_text(int hour, int minute) {
+    // Prepare string for returning, reset used and free counters.
     int used = 0;
     int free = MAXLEN - 1 - used;
     retval[used] = '\0';
 
+    // To make things easier the first eight minutes of the new hour are
+    // handled together with the previous hour. For this the minutes are
+    // shifted back by 8 correcting the hour accordingly.
     minute -= 8;
     if (minute < 0) {
         minute += 60;
         hour -= 1;
     }
-    
+
+    // First divide minutes into 12 sections, for the sections 0, 3, 6, 9 use
+    // "fünf vor", for the sections 2, 5, 8, 11 use "fünf nach".
     int section = minute / 5;
     if (section % 3 == 0)
         addstring(retval, "fünf vor\n", &used, &free);
     if (section % 3 == 2)
         addstring(retval, "fünf nach\n", &used, &free);
-    
+
+    // Now merge the sections into 4 blocks, use "viertel", "halb" and
+    // "dreiviertel" accordingly.
     section = section / 3;
     if (section % 4 == 0)
         addstring(retval, "viertel\n", &used, &free);
@@ -32,7 +43,9 @@ char* german_fuzzy_text(int hour, int minute) {
         addstring(retval, "halb\n", &used, &free);
     if (section % 4 == 2)
         addstring(retval, "dreiviertel\n", &used, &free);
-    
+
+    // This format is inherently 12 hour based, make sure hour is corrected and
+    // printed accordingly.
     if (hour > 12) hour -= 12;
     switch (hour+1) {
         case 0:  addstring(retval, "zwölf", &used, &free); break;
@@ -52,13 +65,3 @@ char* german_fuzzy_text(int hour, int minute) {
 
     return retval;
 }
-
-//int main() {
-//    int h;
-//    for (h = 0; h < 12; h++) {
-//        int m;
-//        for (m = 0; m < 60; m++) {
-//            printf("%d:%d -> \n%s\n", h, m, german_fuzzy_text(h, m));
-//        }
-//    }
-//}
--- src/german_fuzzy_text.h
+++ src/german_fuzzy_text.h
@@ -1,3 +1,5 @@
 #pragma once
 
+// Returns static buffer, which is overwritten with each call, containing a
+// German text for the given hour and minute.
 extern char* german_fuzzy_text(int hour, int minute);
