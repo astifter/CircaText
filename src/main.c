bd149ca56a8f15c334d7eaf4165e31f9b72a2d72
Andreas Neustifter <andreas.neustifter@gmail.com>
2014-10-15 22:33:41 +0200

    Refactorings and comments.

--- src/main.c
+++ src/main.c
@@ -1,25 +1,27 @@
 #include <pebble.h>
 #include "german_fuzzy_text.h"
 
-// Static pointers for window and text layers.
-static Window *s_main_window;
-static TextLayer *s_german_text_layer;
-static TextLayer *s_time_layer;
-static TextLayer *s_date_layer;
+// Static pointers for window and layers.
+static Window      *s_main_window;
+static TextLayer   *s_german_text_layer;
+static TextLayer   *s_time_layer;
+static TextLayer   *s_date_layer;
 static BitmapLayer *s_bg_layer;
-static TextLayer *s_info1_layer;
-static TextLayer *s_info2_layer;
+static TextLayer   *s_info1_layer;
+static TextLayer   *s_info2_layer;
 
+// Keys and variables for persistens storage.
 #define BOTTOMSPACE_KEY 1
 static int bottomspace;
 
-static bool bt_connection;
+// Storage are for phone states.
+static bool               bt_state;
 static BatteryChargeState battery_state;
 
-static char* days[] = { "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So" };
-static char* months[] = { "Jan", "Feb", "Mar", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez" };
+const static char* days[] = { "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So" };
+const static char* months[] = { "Jan", "Feb", "Mar", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez" };
 
-// Updates the text layers displaying the time, date and german text.
+// Updates the layers and writes all sorts of text.
 static void update_time() {
     // Get a tm structure from current time.
     time_t temp = time(NULL);
@@ -52,12 +54,14 @@ static void update_time() {
     // Display date in respective layer.
     text_layer_set_text(s_date_layer, date);
     
+    // Fetch and print BlueTooth status information.
     static char bt_info[80];
     char *statestring;
-    if (bt_connection) statestring = "conn"; else statestring = "disconn";
+    if (bt_state) statestring = "conn"; else statestring = "disconn";
     snprintf(bt_info, 79, "bt: %s", statestring);
     text_layer_set_text(s_info1_layer, bt_info);
 
+    // Fetch and print battery status information.
     static char batt_info[80];
     char* batterystatestring;
     if (!battery_state.is_plugged) batterystatestring = "";
@@ -68,14 +72,17 @@ static void update_time() {
     text_layer_set_text(s_info2_layer, batt_info);
 }
 
+// Handles timer ticks.
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
     // Hand updating to separate method, this method is used at watchface
     // loading as well.
     update_time();
 }
 
+// Handles creation of text layers.
 static TextLayer *create_text_layer(Window *window, GRect r, char *fk) {
-    // Create layer and 
+    // Creates a text layer, sets proper colours, fonts and attaches it to
+    // window.
     TextLayer *l = text_layer_create(r);
     text_layer_set_background_color(l, GColorClear);
     text_layer_set_text_color(l, GColorWhite);
@@ -84,13 +91,18 @@ static TextLayer *create_text_layer(Window *window, GRect r, char *fk) {
     return l;
 }
 
+// Updates the background bitmap layer.
 static void update_bg_layer(struct Layer *layer, GContext *ctx) {
+    // First fill background with black rectangle.
     graphics_context_set_fill_color(ctx, GColorBlack);
     graphics_context_set_stroke_color(ctx, GColorBlack);
     graphics_fill_rect(ctx, layer_get_frame(layer), 0, 0);
     
+    // Switch to white, fetch frame size.
     graphics_context_set_stroke_color(ctx, GColorWhite);
     GRect inforect = layer_get_frame(text_layer_get_layer(s_info1_layer));
+
+    // Draw top and bottom line, use calculated data.
     uint16_t topline = inforect.origin.y + 3;
     graphics_draw_line(ctx, GPoint(inforect.origin.x, topline), 
                             GPoint(inforect.size.w,   topline));
@@ -99,6 +111,7 @@ static void update_bg_layer(struct Layer *layer, GContext *ctx) {
                             GPoint(inforect.size.w,   bottomline));
 }
 
+// Calculates the text height for a given font.
 int16_t get_text_size(char *fk) {
     return graphics_text_layout_get_content_size("00:00", fonts_get_system_font(fk),
                                                  GRect(0,0,100,100), 
@@ -106,6 +119,7 @@ int16_t get_text_size(char *fk) {
                                                  GTextAlignmentLeft).h;
 }
 
+// Loads and sets up main window.
 static void main_window_load(Window *window) {
     GRect screensize = layer_get_bounds(window_get_root_layer(window));
     GRect timerect, daterect, inforect;
@@ -145,6 +159,7 @@ static void main_window_load(Window *window) {
     text_layer_set_text_alignment(s_info2_layer, GTextAlignmentRight);
 }
 
+// Destroy allocated data.
 static void main_window_unload(Window *window) {
     // Destroy text layers in reverse order.
     text_layer_destroy(s_info2_layer);
@@ -155,11 +170,13 @@ static void main_window_unload(Window *window) {
     bitmap_layer_destroy(s_bg_layer);
 }
 
+// Receive BT event and update window.
 void handle_bt_event(bool connected) {
-    bt_connection = connected;
+    bt_state = connected;
     update_time();
 }
 
+// Receive battery evend and update window.
 void handle_battery_event(BatteryChargeState s) {
     battery_state = s;
     update_time();
@@ -169,10 +186,10 @@ static void handle_init(void) {
     // Persistent storage.
     if (persist_exists(BOTTOMSPACE_KEY)) {
         bottomspace = persist_read_int(BOTTOMSPACE_KEY);
-        app_log(APP_LOG_LEVEL_DEBUG, __FILE__, __LINE__, "exisiting BOTTOMSPACE_KEY: %d", bottomspace);
+        //app_log(APP_LOG_LEVEL_DEBUG, __FILE__, __LINE__, "exisiting BOTTOMSPACE_KEY: %d", bottomspace);
     } else {
         bottomspace = 3;
-        app_log(APP_LOG_LEVEL_DEBUG, __FILE__, __LINE__, "new BOTTOMSPACE_KEY: %d", bottomspace);
+        //app_log(APP_LOG_LEVEL_DEBUG, __FILE__, __LINE__, "new BOTTOMSPACE_KEY: %d", bottomspace);
     }
     
     // Create window and add window handlers.
@@ -185,7 +202,7 @@ static void handle_init(void) {
     // Push window onto stack and update text.
     window_stack_push(s_main_window, true);
 
-    bt_connection = bluetooth_connection_service_peek();
+    bt_state = bluetooth_connection_service_peek();
     bluetooth_connection_service_subscribe(handle_bt_event);
     battery_state = battery_state_service_peek();
     battery_state_service_subscribe(handle_battery_event);
