06b5a23161cd899c5c6d9d52de2c07a5c8cc855d
Andreas Neustifter <andreas.neustifter@gmail.com>
2015-01-04 14:05:26 +0100

    Added reference for time-to-text module.

--- src/german_fuzzy_text_test.c
+++ src/german_fuzzy_text_test.c
@@ -6,7 +6,7 @@ int main (int argc, char** argv) {
     for (int hour = 0; hour < 24; hour++) {
         for (int min = 0; min < 60; min++) {
             char* string = german_fuzzy_text(hour, min);
-            printf("%2d:%02d %s\n", hour, min, string);
+            printf("--- %2d:%02d ---\n%s\n", hour, min, string);
         }
     }
     return 0;
--- src/german_fuzzy_text_test.sh
+++ src/german_fuzzy_text_test.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 gcc -o german_fuzzy_text_test german_fuzzy_text_test.c german_fuzzy_text.c
-./german_fuzzy_text_test
+./german_fuzzy_text_test > german_fuzzy_text_test.out
+if ! diff -qs german_fuzzy_text_test.ref german_fuzzy_text_test.out; then
+    diff -u german_fuzzy_text_test.ref german_fuzzy_text_test.out;
+fi
