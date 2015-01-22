1678bb7831f066c15afe0e035220dec5a2c89be5
Andreas Neustifter <andreas.neustifter@gmail.com>
2015-01-22 19:42:45 +0100

    Centralized resources.

---
+++ build_test_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ex
+
+pebble build
+pushd test; ./german_fuzzy_text_test.sh; popd; echo -n "Press [Enter] to install, [Ctrl-C] to exit."; read -n 1 -s;
+pebble install --phone "$1"
+pebble logs --phone "$1"
