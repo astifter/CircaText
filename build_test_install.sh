a1669e12794822daf8759af656b1ae8769b077cf
Andreas Neustifter <andreas.neustifter@gmail.com>
2015-02-03 12:20:19 +0100

    Do not attempt to install w/o IP address.

--- build_test_install.sh
+++ build_test_install.sh
@@ -3,6 +3,8 @@ set -ex
 
 pebble build
 pushd test; ./german_fuzzy_text_test.sh; popd;
+if [[ -n "$1" ]]; then
 echo -n "Press [Enter] to install, [Ctrl-C] to exit."; read -n 1 -s;
 pebble install --phone "$1"
 pebble logs --phone "$1"
+fi
